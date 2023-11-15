03fef040a77df4ae5078542dd194f8d1435514ab
omer1BS <omer1.bs@gmail.com>
2023-11-15 15:58:37 +0200

    moved to sqlmodel added endpoint to create fountain

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,10 @@
 
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey, Boolean, Double,Enum,PickleType
+from sqlalchemy import create_engine, Integer, String, Text, ForeignKey, Boolean,Enum,PickleType
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlmodel import Field,SQLModel,Column,JSON
 from typing import List
 from dotenv import load_dotenv
 import os
@@ -25,53 +26,28 @@ class FountainType(enum.Enum):
 # SQLAlchemy setup
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
-# Database Models
-class FountainModel(Base):
-    __tablename__ = 'fountains'
-    id = Column(Integer, primary_key=True, index=True)
-    location = Column(String(255), index=True)
-    dog_friendly = Column(Boolean)
-    type = Column(Enum(FountainType))
-    average_general_rating= Column(Double())
-    number_of_ratings=Column(Integer)
-
-class ReviewModel(Base):
-    __tablename__ = 'reviews'
-    id = Column(Integer, primary_key=True, index=True)
-    fountain_id = Column(Integer, ForeignKey('fountains.id'))
-    general_rating = Column(Integer)
-    reviewer_nickname = Column(String(60))
-    reviewer_ID =Column(Integer)
-    general_rating=Column(Integer)
-    temp_rating=Column(Integer)
-    stream_rating=Column(Integer)
-    quenching_rating=Column(Integer)
-    description = Column(Text)
-    photos = Column(PickleType())
-
-# Create tables
-Base.metadata.create_all(bind=engine)
-
-# Pydantic Models
-class Fountain(BaseModel):
-    id: int
-    location: str
+# SQLmodel Models - works as pydanic and SQLalchemy models 
+class Fountain(SQLModel, table=True):
+    id: int=Field(primary_key=True,index=True)
+    location: str=Field(index=True)
     dog_friendly: bool
     type:FountainType
     average_general_rating: float
-    number_of_ratings: str
+    number_of_ratings: int
 
-class Review(BaseModel):
-    id: int
-    fountain_id: int
+class Review(SQLModel, table=True):
+    id: int =Field(primary_key=True,index=True)
+    fountain_id: int=Field(index=True,foreign_key='fountain.id')
     general_rating:int
     temp_rating:int
     stream_rating:int
     quenching_rating:int
     description :str
-    photos : List[int]
+    photos : List[int] = Field(sa_column=Column(JSON))
+
+# Create tables
+SQLModel.metadata.create_all(engine)
 
 # FastAPI app
 app = FastAPI()
@@ -86,15 +62,20 @@ def get_db():
 
 @app.get("/fountains", response_model=List[Fountain])
 async def read_fountains(db = Depends(get_db)):
-    return db.query(FountainModel).all()
+    return db.query(Fountain).all()
 
 @app.get("/reviews/{fountain_id}", response_model=List[Review])
 async def read_reviews(fountain_id: int, db = Depends(get_db)):
-    reviews = db.query(ReviewModel).filter(ReviewModel.fountain_id == fountain_id).all()
+    reviews = db.query(Review).filter(Review.fountain_id == fountain_id).all()
     if reviews:
         return reviews
     else:
         raise HTTPException(status_code=404, detail="Fountain not found")
+    
+@app.post("/fountain")
+async def create_fountain(fountain: Fountain,db = Depends(get_db)):
+    db.add(fountain)
+    db.commit() 
 
 if __name__ == "__main__":
     import uvicorn

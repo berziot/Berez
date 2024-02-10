bbb1fdf25154ea4ad2b66c304e870a21b876cece
Ido Nov <idonov8@gmail.com>
2024-02-10 21:53:57 +0700

    Add back button

--- frontend/src/app/[fountain_id]/page.tsx
+++ frontend/src/app/[fountain_id]/page.tsx
@@ -2,11 +2,13 @@
 
 import { Fountain } from "@/app/types";
 import { useFountains } from "@/contexts/FountainContext";
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import {useEffect, useState} from "react";
 import {API_URL} from "@/app/consts";
 
 const FountainPage = () => {
+    const router = useRouter();
+
     const pathname = usePathname();
     const { fountains, setFountains } = useFountains();
     const [currentFountain, setCurrentFountain] = useState<Fountain|null>(null)
@@ -28,20 +30,25 @@ const FountainPage = () => {
     }, [])
 
     return (
-        <div className="card">
-            <h1>ברזייה #{currentFountain?.id}</h1>
-            <br/>
-            <p>כתובת: {currentFountain?.address}</p>
-            <p>
-                ידידותי לכלבים: {currentFountain?.dog_friendly ? 'כן' : 'לא'}
-            </p>
-            <p>
-                דירוג: {currentFountain?.average_general_rating} ({currentFountain?.number_of_ratings} דירוגים)
-            </p>
-            <p>
-                סוג ברזיה: {currentFountain?.type}
-            </p>
-        </div>
+        <>
+            <div className="card">
+                <button className="left button" onClick={() => router.back()}>
+                    &lt;-
+                </button>
+                <h1 className="title">ברזייה #{currentFountain?.id}</h1>
+                <br/>
+                <p>כתובת: {currentFountain?.address}</p>
+                <p>
+                    ידידותי לכלבים: {currentFountain?.dog_friendly ? 'כן' : 'לא'}
+                </p>
+                <p>
+                    דירוג: {currentFountain?.average_general_rating} ({currentFountain?.number_of_ratings} דירוגים)
+                </p>
+                <p>
+                    סוג ברזיה: {currentFountain?.type}
+                </p>
+            </div>
+        </>
     )
 }
 
